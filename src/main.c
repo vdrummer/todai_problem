e13840f0d7f4eadefcc0a683b122b629d2d842a9
Max Depenbrock <maxdepenbrock@posteo.net>
2023-07-01 14:22:23 +0200

    add cairo rendering

--- src/main.c
+++ src/main.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <SDL2/SDL.h>
+#include <cairo/cairo.h>
 
 int main(void) {
   if (SDL_Init(0) < 0) {
@@ -37,6 +38,8 @@ int main(void) {
   }
 
   bool quit = false;
+  cairo_t* cr;
+
   while (!quit) {
     SDL_Event event;
     while (SDL_PollEvent(&event)) {
@@ -47,8 +50,51 @@ int main(void) {
       }
     }
 
+    SDL_Rect windowDimensions = {0};
+    SDL_GetWindowSize(window, &windowDimensions.w, &windowDimensions.h);
+
     SDL_SetRenderDrawColor(r, 255, 255, 255, 255);
     SDL_RenderClear(r);
+
+    SDL_Surface* sdlSurf = SDL_CreateRGBSurface(
+        0,
+        windowDimensions.w,
+        windowDimensions.h,
+        32,
+        0x00ff0000,
+        0x0000ff00,
+        0x000000ff,
+        0x0
+    );
+    if (sdlSurf == NULL) {
+      perror("Could not create RGB Surface");
+      return EXIT_FAILURE;
+    }
+
+    cairo_surface_t* cairoSurf = cairo_image_surface_create_for_data(
+        sdlSurf->pixels,
+        CAIRO_FORMAT_RGB24,
+        windowDimensions.w,
+        windowDimensions.h,
+        sdlSurf->pitch
+    );
+    cr = cairo_create(cairoSurf);
+
+    cairo_set_source_rgb(cr, 0, 0, 0);
+    cairo_paint(cr);
+
+    cairo_surface_destroy(cairoSurf);
+
+    SDL_Texture* sdlTexture = SDL_CreateTextureFromSurface(r, sdlSurf);
+    SDL_FreeSurface(sdlSurf);
+    const SDL_Rect rect = {
+      .w = windowDimensions.w,
+      .h = windowDimensions.h,
+      .x = 0,
+      .y = 0
+    };
+    SDL_RenderCopy(r, sdlTexture, &rect, &rect);
+    SDL_DestroyTexture(sdlTexture);
 
     SDL_RenderPresent(r);
   }
